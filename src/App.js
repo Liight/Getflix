5de489cf07b12971d726e8ff149b52ff69ee5280
Liight <Liight@users.noreply.github.com>
2020-01-20 07:42:55 +1300

    update api logic

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 
 import axios from "axios";
+import { apiKey } from "./secret/secret";
 
 class App extends Component {
   state = {
     hasReceivedMovieData: false,
     movieIdData: [],
-    movieList: []
+    movieList: [],
+    updatedMovieList: [],
+    hasReceivedMovieImageData: false,
+    tempMovieImageData: "initial Value"
   };
 
   componentWillMount() {
@@ -16,7 +20,20 @@ class App extends Component {
     });
   }
 
-  getMovieData = movieIdsArray => { // Internal promise
+  shouldComponentUpdate = (nextProps, nextState) => {
+    return (
+      nextState.hasReceivedMovieData !== this.state.hasReceivedMovieData ||
+      nextState.hasReceivedMovieImageData !==
+        this.state.hasReceivedMovieImageData
+    );
+  };
+
+  componentDidUpdate() {
+    console.log("component updated : ", this.state);
+  }
+
+  getMovieData = movieIdsArray => {
+    // Internal promise
     // Get Movie Data
     let MovieObjectsArray = [];
     let promises = [];
@@ -26,7 +43,7 @@ class App extends Component {
         axios
           .get("https://api.themoviedb.org/3/movie/" + movieIdsArray[i] + "?", {
             params: {
-              api_key: "c50edcff58bb0a25f1aa137a9751550d",
+              api_key: apiKey,
               language: "en-US"
             }
           })
@@ -43,22 +60,29 @@ class App extends Component {
     Promise.all(promises).then(() =>
       this.setState(
         prevState => {
-          return { ...prevState, movieList: MovieObjectsArray };
+          return {
+            ...prevState,
+            movieList: MovieObjectsArray,
+            hasReceivedMovieData: true
+          };
         },
         () => {
-          console.log(this.state);
+          console.log("state after getMovieData: ", this.state);
+          // Update Image Data on created movie object in State
+          this.callGetMovieImageData(MovieObjectsArray);
         }
       )
     );
   };
 
-  getMovieIdsList = async () => { // Async / Await
+  getMovieIdsList = async () => {
+    // Async / Await
     // Get some movie Ids (changes)
     let movieIds = [];
     await axios
       .get("https://api.themoviedb.org/3/movie/top_rated?", {
         params: {
-          api_key: "c50edcff58bb0a25f1aa137a9751550d",
+          api_key: apiKey,
           language: "en-US",
           page: 1
         }
@@ -76,17 +100,87 @@ class App extends Component {
     this.setState(prevState => {
       return {
         ...prevState,
-        hasReceivedMovieData: true,
         movieIdData: movieIds
       };
     });
   };
 
+  getMovieDataList = () => {
+    let movieList = [...this.state.movieList];
+    // console.log('getMovieDataList : movieList', movieList);
+    return movieList;
+  };
+
+  callGetMovieImageData = MovieObjectsArray => {
+    let prefix = "https://image.tmdb.org/t/p/original";
+    let moviesWithImages = [...MovieObjectsArray];
+    let promises = [];
+
+    for (let i = 0; i < moviesWithImages.length - 1; i++) {
+      promises.push(
+        axios
+          .get(
+            "https://api.themoviedb.org/3/movie/" +
+              moviesWithImages[i].id +
+              "/images?",
+            {
+              params: {
+                api_key: apiKey,
+                page: 1
+              }
+            }
+          )
+          .then(response => {
+            moviesWithImages[i].posterUrl =
+              prefix + response.data.posters[0].file_path;
+          })
+          .catch(error => {
+            console.log(error);
+          })
+      );
+    }
+
+    Promise.all(promises).then(() =>
+      this.setState(
+        prevState => {
+          return {
+            ...prevState,
+            hasReceivedMovieImageData: true,
+            updatedMovieList: moviesWithImages
+          };
+        },
+        () => {
+          setTimeout(() => {
+            console.log("state after updateMovieImageData: ", this.state);
+          }, 1000);
+        }
+      )
+    );
+  };
+
   render() {
+    console.log("getMovieDataList: initial render", this.getMovieDataList());
+    const moviesArray = this.getMovieDataList();
+
+    let movieRow = moviesArray.map(movie => {
+      return (
+        <div id="movie-row-item" key={Math.random() * 10}>
+          <p>{movie.title}</p>
+          <p>{movie.overview}</p>
+          <img
+            src={movie.posterUrl}
+            width={"auto"}
+            height={"100px"}
+            alt=""
+          ></img>
+        </div>
+      );
+    });
 
     return (
       <div className="App">
         <h1>running...</h1>
+        {movieRow}
       </div>
     );
   }
