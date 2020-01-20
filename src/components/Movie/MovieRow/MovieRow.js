1958625060e782e2de7e4d4d8fe958c27cd0fd26
Liight <Liight@users.noreply.github.com>
2020-01-20 19:15:25 +1300

    dynamic rendering

--- src/components/Movie/MovieRow/MovieRow.js
+++ src/components/Movie/MovieRow/MovieRow.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./MovieRow.css";
 
 import MovieRowItem from "../MovieRowItem/MovieRowItem";
@@ -8,24 +8,59 @@ import MovieRowItem from "../MovieRowItem/MovieRowItem";
 const MovieRow = props => {
   console.log("props", props);
 
-  const handleClick = e => {
-    console.log(e.id);
-    switch(e.id){
-        case "left":
-
-            break;
-        case "right":
-                
-            break;
-        default: 
-        return;
-    }
+  // Dynamic Refs
+  const ref = React.useRef(null);
+  let scrollerRefs = [];
+  const setRef = ref => {
+    scrollerRefs.push(ref);
   };
+  let focusedSection = 0;
+  const focusInput = id => {
+    console.log("scrollerRefs", scrollerRefs);
+    if(id < 0){
+      id = 0;
+    } else if (id >= scrollerRefs.length - 1){
+      id = scrollerRefs.length - 1;
+    }
+    scrollerRefs[id].scrollIntoView();
+    focusedSection = id;
+  };
+  
+
+  // Define movie row sections as an array of sub arrays
+  let numberOfMovieRowSections = 0;
+  let movieRowSections = [];
+  if (props.movies.length > 0) {
+    let tempChunkSize = 3;
+    let chunk = (arr, chunkSize) => {
+      var R = [];
+      for (var i = 0, len = arr.length; i < len; i += chunkSize) {
+        R.push(arr.slice(i, i + chunkSize));
+        numberOfMovieRowSections += 1;
+      }
+
+      return R;
+    };
+
+    movieRowSections = chunk(props.movies, tempChunkSize);
+    console.log("movieRowSections : FINAL : ", movieRowSections);
+  }
 
   let movieRowItems =
-    props.movies.length > 0 ? (
-      props.movies.map(movie => {
-        return <MovieRowItem key={movie.id} movie={movie} />;
+    movieRowSections.length > 0 ? (
+      movieRowSections.map((section, index) => {
+        return (
+          <div
+            id={index.toString()}
+            className="movie-row-section"
+            key={Math.random() * 10}
+            ref={setRef}
+          >
+            {section.map(movie => {
+              return <MovieRowItem key={Math.random() * 10} movie={movie} />;
+            })}
+          </div>
+        );
       })
     ) : (
       <p>Loading</p>
@@ -35,13 +70,14 @@ const MovieRow = props => {
     <div className="movie-row-container">
       <div className="movie-row">
         <div className="leftArrow">
-          <span id="left" onClick={(event) => handleClick(event.target)}>
+          {/* <span id="left" onClick={event => handleClick(event.target)}> */}
+          <span id="left" onClick={() => focusInput(focusedSection - 1)}>
             leftArrow
           </span>
         </div>
         <div className="movie-row-items-container">{movieRowItems}</div>
         <div className="rightArrow">
-          <span id="right" onClick={event => handleClick(event.target)}>
+          <span id="right" onClick={() => focusInput(focusedSection+1)}>
             rightArrow
           </span>
         </div>
