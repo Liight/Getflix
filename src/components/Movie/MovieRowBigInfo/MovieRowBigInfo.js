848661b9829408b28b8a9df9082fd0a30197166c
Liight <Liight@users.noreply.github.com>
2020-01-22 12:57:46 +1300

    layout updates

--- src/components/Movie/MovieRowBigInfo/MovieRowBigInfo.js
+++ src/components/Movie/MovieRowBigInfo/MovieRowBigInfo.js
@@ -4,32 +4,47 @@ import "./MovieRowBigInfo.css";
 const MovieRowBigInfo = props => {
   console.log("movie in movieitem : ", props.movie);
   if (props.movie === "") {
-    return null;
+    return <div className="movie-row-big-info-wrapper no-reveal"></div>
   } else {
     return (
-      <div className="movie-row-big-info-container">
-        <div className="info-column">
-          <span className="title">{props.movie.title}</span>
-          <span className="tagline">{props.movie.tagline}</span>
-          <span>
-            <span className="status">{props.movie.status + " "}</span>
-            <span className="release-date">
-              {props.movie.release_date.length > 0 ? props.movie.release_date.slice(0, 4) + " " : null}
+      <div className="movie-row-big-info-wrapper">
+        <div className="movie-row-big-info-container reveal">
+          <div
+            className="info-column"
+            style={{
+              paddingLeft: window.innerWidth / 12
+            }}
+          >
+            <span className="title">{props.movie.title}</span>
+            <span className="tagline">{props.movie.tagline}</span>
+            <span>
+              <span className="status">{props.movie.status + " "}</span>
+              <span className="release-date">
+                {props.movie.release_date.length > 0
+                  ? props.movie.release_date.slice(0, 4) + " "
+                  : null}
+              </span>
+              <span className="runtime"> {props.movie.runtime} mins</span>
             </span>
-            <span className="runtime"> {props.movie.runtime} mins</span>
-          </span>
-          {/* <span>
+            {/* <span>
             <span className="runtime"> budget ${props.movie.budget}</span>
             <span className="runtime"> revenue ${props.movie.revenue}</span>
           </span> */}
-          {/* <span className="runtime">{props.movie.runtime}</span> */}
-          <span className="overview">{props.movie.overview}</span>
+            {/* <span className="runtime">{props.movie.runtime}</span> */}
+            <span className="overview">{props.movie.overview}</span>
+          </div>
+          <div
+            className="image-column"
+            // style={{ backgroundImage: "url(" + props.movie.posterUrl + ")" }}
+          >
+            <img
+              src={props.movie.posterUrl}
+              alt=""
+              height="100%"
+              width="auto"
+            />
+          </div>
         </div>
-        <div
-          className="image-column"
-          // style={{ backgroundImage: "url(" + props.movie.posterUrl + ")" }}
-          
-        ><img src={props.movie.posterUrl} alt="" height="100%" width="auto"/></div>
       </div>
     );
   }
