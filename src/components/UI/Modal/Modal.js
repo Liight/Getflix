4e81bcb4bdd4c1d81d6b88cfce87be8a6360bae4
Liight <Liight@users.noreply.github.com>
2020-01-25 14:55:57 +1300

    behavious

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -22,61 +22,85 @@ const Modal = props => {
       </div>
       <div className="modal-inner-display">
         <div className="modal-inner-display-info">
-          <div className="">{props.movie.title}</div>
-          <div className="">{props.movie.adult}</div>
-          <div className="">{props.movie.backdrop_path}</div>
-          <div className="">{props.movie.belongs_to_collection}</div>
-          <div className="">{props.movie.budget}</div>
-          <div className="">{props.movie.homepage}</div>
-          <div className="">{props.movie.id}</div>
-          <div className="">{props.movie.imdb_id}</div>
-          <div className="">{props.movie.original_language}</div>
-          <div className="">{props.movie.original_title}</div>
-          <div className="">{props.movie.overview}</div>
-          <div className="">{props.movie.popularity}</div>
-          <div className="">{props.movie.poster_path}</div>
-          <div className="">{props.movie.release_date}</div>
-          <div className="">{props.movie.revenue}</div>
-          <div className="">{props.movie.runtime}</div>
-          <div className="">{props.movie.status}</div>
-          <div className="">{props.movie.tagline}</div>
-          <div className="">{props.movie.title}</div>
-          <div className="">{props.movie.video}</div>
-          <div className="">{props.movie.vote_average}</div>
-          <div className="">{props.movie.vote_count}</div>
-          <div className="">{props.movie.posterUrl}</div>
+          <div className="modal-flex-row modal-text">{"title: "+props.movie.title}</div>
+          {/* <div className="">{props.movie.adult}</div> */}
+          {/* <div className="">{props.movie.backdrop_path}</div> */}
+          {/* <div className="">{props.movie.belongs_to_collection}</div> */}
+          <div className="modal-flex-row modal-text">
+            {"budget: " + props.movie.budget}
+          </div>
+          <div className="modal-flex-row modal-text">
+            {"homepage: " + props.movie.homepage}
+          </div>
+          {/* <div className="">{props.movie.id}</div> */}
+          {/* <div className="">{props.movie.imdb_id}</div> */}
+          <div className="modal-flex-row modal-text">
+            {"original language: " + props.movie.original_language}
+          </div>
+          <div className="modal-flex-row modal-text">
+            {"original title: " + props.movie.original_title}
+          </div>
+          <div className="modal-flex-row modal-text">
+            {"overview: " + props.movie.overview}
+          </div>
+          <div className="modal-flex-row modal-text">
+            {"popularity: " + props.movie.popularity}
+          </div>
+          {/* <div className="">{props.movie.poster_path}</div> */}
+          <div className="modal-flex-row modal-text">
+            {"release date: " + props.movie.release_date}
+          </div>
+          <div className="modal-flex-row modal-text">
+            {"revenue: " + props.movie.revenue}
+          </div>
+          <div className="modal-flex-row modal-text">
+            {"runtime: " + props.movie.runtime}
+          </div>
+          <div className="modal-flex-row modal-text">
+            {"status: " + props.movie.status}
+          </div>
+          <div className="modal-flex-row modal-text">
+            {"tagline: " + props.movie.tagline}
+          </div>
+          {/* <div className="">{props.movie.video}</div> */}
+          <div className="modal-flex-row modal-text">
+            {"vote average: " + props.movie.vote_average}
+          </div>
+          <div className="modal-flex-row modal-text">
+            {"vote count: " + props.movie.vote_count}
+          </div>
+          {/* <div className="">{props.movie.posterUrl}</div> */}
 
-          <div className="">
+          <div className="modal-flex-row modal-text">
+            {"Genres: "}
             {props.movie.genres.map(g => {
-              console.log(g);
-              return <div className="">{"Genres"}</div>;
+              return <div className="">{g.name + ", "}</div>;
             })}
           </div>
 
-          <div className="">
-            {props.movie.production_companies.map(g => {
-              console.log(g);
-              return <div className="">{"production_companies"}</div>;
+          <div className="modal-flex-row modal-text">
+            {"Production Companies: "}
+            {props.movie.production_companies.map(pc => {
+              return <div className="">{pc.name + ", "}</div>;
             })}
           </div>
 
-          <div className="">
-            {props.movie.production_countries.map(g => {
-              console.log(g);
-              return <div className="">{"production_countries"}</div>;
+          <div className="modal-flex-row modal-text">
+            {"Production Countries: "}
+            {props.movie.production_countries.map(pc => {
+              return <div className="">{pc.name}</div>;
             })}
           </div>
 
-          <div className="">
-            {props.movie.spoken_languages.map(g => {
-              console.log(g);
-              return <div className="">{"spoken_languages"}</div>;
+          <div className="modal-flex-row modal-text">
+            {"Spoken Languages: "}
+            {props.movie.spoken_languages.map(sl => {
+              return <div className="">{sl.name + ", "}</div>;
             })}
           </div>
-
         </div>
         <div className="modal-inner-display-image">
-          <img src={""} alt=""></img>
+          <img src={props.movie.posterUrl} alt=""></img>
         </div>
       </div>
     </div>
@@ -96,7 +120,6 @@ const mapDispatchToProps = dispatch => {
   return {
     onToggleModal: () => dispatch(actions.toggleModal()),
     onToggleGlobalScrollbars: () => dispatch(actions.toggleGlobalScrollbars()) // follow this thread
-
   };
 };
 
