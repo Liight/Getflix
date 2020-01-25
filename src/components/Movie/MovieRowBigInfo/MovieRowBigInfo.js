9d662da593d73de89ad0f451cefd8dba5610ca11
Liight <Liight@users.noreply.github.com>
2020-01-25 14:12:56 +1300

    layout and styles

--- src/components/Movie/MovieRowBigInfo/MovieRowBigInfo.js
+++ src/components/Movie/MovieRowBigInfo/MovieRowBigInfo.js
@@ -7,27 +7,23 @@ import "./MovieRowBigInfo.css";
 
 class MovieRowBigInfo extends Component {
   state = {
-    // thisInitialKey: this.props.activeBigInfoKey
-  };
 
-  // shouldComponentUpdate = (nextProps, nextState) => {
-  //   console.log("BIG INFO component should update");
-  //   return nextProps.activeBigInfoKey === this.state.thisInitialKey;
-  // };
+  };
 
   render() {
     console.log(
       " movie row big info rendered",
-      "key from movie rows display ", this.props.thisRowsBigInfoKey,
-      "global state active key ",this.props.activeBigInfoKey,
-      // this.state.thisInitialKey,
+      "key from movie rows display ",
+      this.props.thisRowsBigInfoKey,
+      "global state active key ",
+      this.props.activeBigInfoKey,
       this.props.movie
     );
 
     let BigInfo =
       this.props.movie === "" ||
       this.props.thisRowsBigInfoKey !== this.props.activeBigInfoKey
-        ? // this.props.thisRowsBigInfoKey !== this.props.activeBigInfoKey
+        ? 
           (console.log("else: dont show"),
           (<div className="movie-row-big-info-wrapper no-reveal"></div>))
         : (console.log("evaluating to show"),
@@ -41,37 +37,47 @@ class MovieRowBigInfo extends Component {
                   }}
                 >
                   <div className="info-column-top">
-                    <span className="title">{this.props.movie.title}</span>
-                    <span className="tagline">{this.props.movie.tagline}</span>
-                    <span>
-                      <span className="status">
-                        {this.props.movie.status + " "}
-                      </span>
-                      {/* // Throws error here iterating over undefined */}
-                      <span className="release-date">
-                        {this.props.movie.release_date.length > 0
-                          ? this.props.movie.release_date.slice(0, 4) + " "
-                          : null}
-                      </span>
+                    <div
+                      class="table-container"
+                      role="table"
+                      aria-label="Destinations"
+                    >
+                      <div class="flex-row title">{this.props.movie.title}</div>
+                      <div class="flex-row tagline">
+                        {this.props.movie.tagline}
+                      </div>
 
-                      <span className="runtime">
-                        {" "}
-                        {this.props.movie.runtime} mins
-                      </span>
-                    </span>
-                    {/* <span>
-            <span className="runtime"> budget ${this.props.movie.budget}</span>
-            <span className="runtime"> revenue ${this.props.movie.revenue}</span>
-          </span> */}
-                    {/* <span className="runtime">{this.props.movie.runtime}</span> */}
-                    <span className="overview">
-                      {this.props.movie.overview}
-                    </span>
+                      <div class="flex-row multi-on-line">
+                        <div class="flex-row status">
+                          {this.props.movie.status + " "}
+                          {this.props.movie.release_date.length > 0
+                            ? this.props.movie.release_date.slice(0, 4) + " "
+                            : null}
+                        </div>
+                        <div class="flex-row runtime">
+                          {" " + this.props.movie.runtime + "mins"}
+                        </div>
+                        <div class="flex-row rating">
+                          {"popularity: " + this.props.movie.popularity}
+                        </div>
+                        <div class="flex-row rating">
+                          {"voteAv: " + this.props.movie.vote_average}
+                        </div>
+                      </div>
+
+                      <div class="flex-row overview">
+                        {this.props.movie.overview}
+                      </div>
+                    </div>
                   </div>
+
                   <div className="info-column-bottom">
                     <button
                       style={{ height: "50px", width: "100px" }}
-                      onClick={() => {this.props.onToggleModal(); this.props.onToggleGlobalScrollbars();}}
+                      onClick={() => {
+                        this.props.onToggleModal();
+                        this.props.onToggleGlobalScrollbars();
+                      }}
                     >
                       More Info
                     </button>
@@ -79,7 +85,6 @@ class MovieRowBigInfo extends Component {
                 </div>
                 <div
                   className="image-column"
-                  // style={{ backgroundImage: "url(" + this.props.movie.posterUrl + ")" }}
                 >
                   <img
                     src={this.props.movie.posterUrl}
